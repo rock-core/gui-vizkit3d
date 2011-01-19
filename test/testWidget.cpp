ae32c77ee10c3a39c8eb5b4e325716a68e3d23d7
Sascha Arnold <sascha.arnold@dfki.de>
2011-01-19 17:20:07 +0100

    added tests of the vizualisation plugins and the changeCameraView method

--- test/testWidget.cpp
+++ test/testWidget.cpp
@@ -1,45 +1,121 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+#define BOOST_TEST_MODULE "test"
+#define BOOST_AUTO_TEST_MAIN
+
 #include <vizkit/QVizkitWidget.hpp>
 #include <vizkit/QtThreadedWidget.hpp>
 #include <vizkit/TrajectoryVisualization.hpp>
+#include <vizkit/WaypointVisualization.hpp>
+#include <vizkit/MotionCommandVisualization.hpp>
+#include <boost/test/unit_test.hpp>
 
 namespace vizkit
 {
-class QTrajectoryWidget : public QVizkitWidget
+    
+template <class T, class D>
+class QVisualisationTestWidget : public QVizkitWidget
 {
     public:
-	QTrajectoryWidget( QWidget* parent = 0, Qt::WindowFlags f = 0 )
-	    : QVizkitWidget( parent, f ), trajViz( new TrajectoryVisualization() )
-	{
-	    addDataHandler( trajViz.get() );
-	}
+        QVisualisationTestWidget( QWidget* parent = 0, Qt::WindowFlags f = 0 )
+            : QVizkitWidget( parent, f ), viz(new T())
+        {
+            addDataHandler( viz.get() );
+        }
 
-	~QTrajectoryWidget()
-	{
-	    removeDataHandler( trajViz.get() );
-	}
+        ~QVisualisationTestWidget()
+        {
+            removeDataHandler( viz.get() );
+        }
 
-	void addPosition( const Eigen::Vector3d& point )
-	{
-	    trajViz->updateData( point );
-	}
-
+        void updateData( const D &data )
+        {
+            viz->updateData(data);
+        }
+      
     private:
-	boost::shared_ptr<TrajectoryVisualization> trajViz;
+        boost::shared_ptr<T> viz;
 };
+
 }
 
-int main( int argc, char **argv )
-{
-    QtThreadedWidget<vizkit::QTrajectoryWidget> app;
-    app.start();
+BOOST_AUTO_TEST_SUITE(vizkit_test)
 
+/** Test of the TrajectoryVisualization */
+BOOST_AUTO_TEST_CASE(trajectoryVisualization_test) 
+{
+    std::cout << "Testing TrajectoryVisualization" << std::endl;
+    QtThreadedWidget<vizkit::QVisualisationTestWidget<vizkit::TrajectoryVisualization, Eigen::Vector3d> > app;
+    app.start();
+    std::cout << "Close the visualization window to abort this test." << std::endl;
     for( int i=0; i<10000 && app.isRunning(); i++ )
     {
-	double r = i/1000.0;
-	double s = r/10;
-	app.widget->addPosition( Eigen::Vector3d( cos(r) * s, sin(r) * s, s ) );
-
-	usleep( 1000 );
+        double r = i/1000.0;
+        double s = r/10;
+        app.widget->updateData( Eigen::Vector3d( cos(r) * s, sin(r) * s, s ) );
+        
+        usleep( 1000 );
     }
 }
 
+/** Test of the WaypointVisualization */
+BOOST_AUTO_TEST_CASE(waypointVisualization_test) 
+{
+    std::cout << "Testing WaypointVisualization" << std::endl;
+    QtThreadedWidget<vizkit::QVisualisationTestWidget<vizkit::WaypointVisualization, base::Waypoint> > app;
+    app.start();
+    std::cout << "Close the visualization window to abort this test." << std::endl;
+    for( int i=0; i<10000 && app.isRunning(); i++ )
+    {
+        double r = i/1000.0;
+        double s = r/10;
+        app.widget->updateData( base::Waypoint(Eigen::Vector3d( cos(r) * r, sin(r) * r, s), s, s*r, s ));
+        
+        usleep( 1000 );
+    }
+}
+
+/** Test of the MotionCommandVisualization */
+BOOST_AUTO_TEST_CASE(motionCommandVisualization_test)
+{
+    std::cout << "Testing MotionCommandVisualization" << std::endl;
+    QtThreadedWidget<vizkit::QVisualisationTestWidget<vizkit::MotionCommandVisualization, std::pair<double, double> > > app;
+    app.start();
+    std::cout << "Close the visualization window to abort this test." << std::endl;
+    for( int i=0; i<10000 && app.isRunning(); i++ )
+    {
+        double r = i/1000.0;
+        double s = r/10;
+        app.widget->updateData(std::pair<double, double>(s, sin(r) * r));
+        
+        usleep( 1000 );
+    }
+}
+
+/** Test of the changeCameraView method */
+BOOST_AUTO_TEST_CASE(changeCameraView_test) 
+{
+    std::cout << "Testing changeCameraView method" << std::endl;
+    QtThreadedWidget<vizkit::QVisualisationTestWidget<vizkit::TrajectoryVisualization, Eigen::Vector3d> > app;
+    app.start();
+    std::cout << "Close the visualization window to abort this test." << std::endl;
+    for( int i=0; i<30000 && app.isRunning(); i++ )
+    {
+        double r = i/1000.0;
+        double s = r/10;
+        app.widget->changeCameraView(osg::Vec3d(cos(r)*s,sin(r)*s,0));
+        
+        usleep( 500 );
+    }
+    
+    for( int i=0; i<30000 && app.isRunning(); i++ )
+    {
+        double r = i/1000.0;
+        double s = r/10;
+        app.widget->changeCameraView(osg::Vec3d(cos(r)*s,sin(r)*s,0), osg::Vec3d(cos(r)*s,(sin(r)*s)-20,20));
+        
+        usleep( 500 );
+    } 
+}
+
+BOOST_AUTO_TEST_SUITE_END();
