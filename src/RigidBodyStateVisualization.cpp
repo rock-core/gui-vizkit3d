ae6f17fad919159e9308d95edbdcc47910280e2f
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-05-11 15:15:59 +0200

    in the RigidBodyState visualizer, fix display of uncertainty and make it more configurable

--- src/RigidBodyStateVisualization.cpp
+++ src/RigidBodyStateVisualization.cpp
@@ -11,34 +11,36 @@ namespace vizkit
 RigidBodyStateVisualization::RigidBodyStateVisualization()
 {
     VizPluginRubyAdapter(RigidBodyStateVisualization, base::samples::RigidBodyState, RigidBodyState)
+    VizPluginRubyConfig(RigidBodyStateVisualization, double, resetModel);
+    VizPluginRubyConfig(RigidBodyStateVisualization, bool, displayCovarianceWithSamples);
 }
 
 RigidBodyStateVisualization::~RigidBodyStateVisualization()
 {
 }
 
-osg::ref_ptr<osg::Geode> RigidBodyStateVisualization::createSimpleBody()
+osg::ref_ptr<osg::Geode> RigidBodyStateVisualization::createSimpleBody(double size)
 {   
     osg::ref_ptr<osg::Geode> geode = new osg::Geode();
 
-    osg::ref_ptr<osg::Sphere> sp = new osg::Sphere(osg::Vec3f(0,0,0), 0.3);
+    osg::ref_ptr<osg::Sphere> sp = new osg::Sphere(osg::Vec3f(0,0,0), size / 10);
     osg::ref_ptr<osg::ShapeDrawable> spd = new osg::ShapeDrawable(sp);
     geode->addDrawable(spd);
     
     //up
-    osg::ref_ptr<osg::Cylinder> c1 = new osg::Cylinder(osg::Vec3f(0, 0, 0.5), 0.05, 1.0);
+    osg::ref_ptr<osg::Cylinder> c1 = new osg::Cylinder(osg::Vec3f(0, 0, size / 2), size / 40, size);
     osg::ref_ptr<osg::ShapeDrawable> c1d = new osg::ShapeDrawable(c1);
     geode->addDrawable(c1d);
     
     //north direction
-    osg::ref_ptr<osg::Cylinder> c2 = new osg::Cylinder(osg::Vec3f(0, 0.5, 0), 0.05, 1.0);
+    osg::ref_ptr<osg::Cylinder> c2 = new osg::Cylinder(osg::Vec3f(0, size / 2, 0), size / 40, size);
     c2->setRotation(osg::Quat(M_PI/2.0, osg::Vec3d(1,0,0)));
     osg::ref_ptr<osg::ShapeDrawable> c2d = new osg::ShapeDrawable(c2);
     c2d->setColor(osg::Vec4f(1.0, 0, 0, 1.0));
     geode->addDrawable(c2d);
   
     //east
-    osg::ref_ptr<osg::Cylinder> c3 = new osg::Cylinder(osg::Vec3f(0.5, 0, 0), 0.05, 1.0);
+    osg::ref_ptr<osg::Cylinder> c3 = new osg::Cylinder(osg::Vec3f(size / 2, 0, 0), size / 40, size);
     c3->setRotation(osg::Quat(M_PI/2.0, osg::Vec3d(0,1,0)));
     osg::ref_ptr<osg::ShapeDrawable> c3d = new osg::ShapeDrawable(c3);
     geode->addDrawable(c3d);
@@ -46,9 +48,9 @@ osg::ref_ptr<osg::Geode> RigidBodyStateVisualization::createSimpleBody()
     return geode;
 }
 
-void RigidBodyStateVisualization::resetModel()
+void RigidBodyStateVisualization::resetModel(double size)
 {
-    body_model = createSimpleBody();
+    body_model = createSimpleBody(size);
 }
 
 void RigidBodyStateVisualization::loadModel(std::string const& path)
@@ -57,27 +59,45 @@ void RigidBodyStateVisualization::loadModel(std::string const& path)
     body_model = model;
 }
 
+void RigidBodyStateVisualization::displayCovarianceWithSamples(bool enable)
+{
+    covariance_with_samples = enable;
+}
+
 osg::ref_ptr<osg::Node> RigidBodyStateVisualization::createMainNode()
 {
-    osg::PositionAttitudeTransform* pose = new osg::PositionAttitudeTransform();
-    pose->addChild(new Uncertainty);
-    body_model = createSimpleBody();
-    return pose;
+    osg::Group* group = new osg::Group;
+    group->addChild(new Uncertainty);
+
+    osg::PositionAttitudeTransform* body_pose =
+        new osg::PositionAttitudeTransform();
+    if (!body_model)
+        resetModel(1);
+    body_pose->addChild(body_model);
+    group->addChild(body_pose);
+    return group;
 }
 
 void RigidBodyStateVisualization::updateMainNode(osg::Node* node)
 {
-    osg::PositionAttitudeTransform* pose =
-        dynamic_cast<osg::PositionAttitudeTransform*>(node->asTransform());
+    osg::Group* group = node->asGroup();
+
     Uncertainty* uncertainty =
-        dynamic_cast<Uncertainty*>(pose->getChild(0));
-    osg::Node* body_node = pose->getChild(1);
+        dynamic_cast<Uncertainty*>(group->getChild(0));
+    if (covariance_with_samples)
+        uncertainty->showSamples();
+    else
+        uncertainty->hideSamples();
+    osg::PositionAttitudeTransform* body_pose =
+        dynamic_cast<osg::PositionAttitudeTransform*>(group->getChild(1));
+
+    osg::Node* body_node = body_pose->getChild(0);
     if (body_node != this->body_model)
-        pose->setChild(1, this->body_model);
+        body_pose->setChild(0, this->body_model);
 
     if(state.hasValidPosition()) {
 	pos.set(state.position.x(), state.position.y(), state.position.z());
-	pose->setPosition(pos);
+	body_pose->setPosition(pos);
         uncertainty->setMean(static_cast<Eigen::Vector3d>(state.position));
         uncertainty->setCovariance(static_cast<Eigen::Matrix3d>(state.cov_position));
     }
@@ -86,7 +106,7 @@ void RigidBodyStateVisualization::updateMainNode(osg::Node* node)
                 state.orientation.y(),
                 state.orientation.z(),
                 state.orientation.w());
-        pose->setAttitude(orientation);
+        body_pose->setAttitude(orientation);
     }
 }
 
--- src/RigidBodyStateVisualization.hpp
+++ src/RigidBodyStateVisualization.hpp
@@ -20,16 +20,18 @@ class RigidBodyStateVisualization : public VizPlugin<base::samples::RigidBodySta
 	virtual void updateMainNode(osg::Node* node);
 	void updateDataIntern( const base::samples::RigidBodyState& state );
         base::samples::RigidBodyState state;
-        void resetModel();
+        void resetModel(double size);
         void loadModel(std::string const& path);
+        void displayCovarianceWithSamples(bool enable);
 	
     private:
+        bool covariance_with_samples;
 	osg::Vec3d pos;
 	osg::Quat orientation;
 
 	osg::ref_ptr<osg::Node> body_model;
 
-        osg::ref_ptr<osg::Geode> createSimpleBody();
+        osg::ref_ptr<osg::Geode> createSimpleBody(double size);
 };
 
 }
