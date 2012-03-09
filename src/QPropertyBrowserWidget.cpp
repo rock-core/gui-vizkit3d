fa936855cc105a622dc6299512f414784bdc1b2a
Alexander Duda <Alexander.Duda@dfki.de>
2012-03-09 20:35:46 +0100

    vizkit PropertyBrowser: do not expand by default
    -> if multiple plugins are loaded it looks quite ugly if all
    properties are expanded

--- src/QPropertyBrowserWidget.cpp
+++ src/QPropertyBrowserWidget.cpp
@@ -113,7 +113,10 @@ void QProperyBrowserWidget::addProperties(QObject* obj,QObject* parent)
 
     // do not add property if it is already added to a parent_group
     if(!parent_group)
-        this->addProperty(group);
+    {
+        QtBrowserItem *item = this->addProperty(group);
+        setExpanded(item,false);
+    }
     
     // connect plugin signal, to notice if a property has changed
     if (!this->connect(obj, SIGNAL(propertyChanged(QString)), this, SLOT(propertyChangedInObject(QString))))
