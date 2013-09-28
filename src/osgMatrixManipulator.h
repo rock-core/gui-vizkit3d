4e2a6f3634232b7e74ea33d846a91c9ac946785a
Alexander Duda <Alexander.Duda@dfki.de>
2013-09-28 02:50:25 +0200

    MatrixManipulator has been renamed to CameraManipulator in OSG 3, add a compatibility header
    Conflicts:
    	src/osgMatrixManipulator.h

--- src/osgMatrixManipulator.h
+++ src/osgMatrixManipulator.h
@@ -3,15 +3,11 @@
 
 #include <osg/Version>
 
-#if defined(OSG_MIN_VERSION_REQUIRED)
 #if OSG_MIN_VERSION_REQUIRED(3,0,0)
 #include <osgGA/CameraManipulator>
 namespace osgGA {
     typedef CameraManipulator MatrixManipulator;
 }
-#else
-#include <osgGA/MatrixManipulator>
-#endif
 #else
 #include <osgGA/MatrixManipulator>
 #endif
