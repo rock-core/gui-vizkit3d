db1effa091b5dad7fbfd49ebc5ebad500586f779
Alexander Duda <Alexander.Duda@dfki.de>
2013-10-06 14:18:36 +0200

    add simple ruby script displaying the main widget

---
+++ scripts/show.rb
@@ -0,0 +1,20 @@
+begin
+    require 'pp'
+    require 'Qt4'
+    require 'qtuitools'
+rescue Exception => e
+    log = Logger.new(STDOUT)
+    log.error "!!! Cannot load Qt4 ruby bindings !!!"
+    raise e
+end
+
+app = Qt::Application.new(ARGV)
+loader = Qt::UiLoader.new()
+widget = loader.createWidget("vizkit3d::Vizkit3DWidget")
+plugin = widget.loadPlugin("vizkit3d","GridVisualization")
+widget.show
+
+pp widget.getAvailablePlugins
+app.exec
+
+
