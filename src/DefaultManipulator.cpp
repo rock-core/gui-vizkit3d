71b63acad3b3b9bcf0f197e6a4fbc2d643012673
Sylvain Joyeux <sylvain.joyeux@fieb.org.br>
2014-12-23 14:16:42 -0200

    create the default manipulator

---
+++ src/DefaultManipulator.cpp
@@ -0,0 +1,20 @@
+#include <vizkit3d/DefaultManipulator.hpp>
+
+using namespace vizkit3d;
+
+DefaultManipulator::DefaultManipulator()
+{
+    setWheelMovement(10.0, false);
+}
+
+// doc in parent
+bool DefaultManipulator::performMovementRightMouseButton( const double eventTimeDelta, const double dx, const double dy )
+{
+    // pan model
+    float scale = 10.0f;
+
+    moveRight(-scale * dx);
+    moveUp(scale * dy);
+    return true;
+}
+
--- src/Vizkit3DWidget.cpp
+++ src/Vizkit3DWidget.cpp
@@ -26,6 +26,7 @@
 #include <osgQt/GraphicsWindowQt>
 #include <osgViewer/ViewerEventHandlers>
 
+#include <vizkit3d/DefaultManipulator.hpp>
 #include <osgGA/FirstPersonManipulator>
 #include <osgGA/FlightManipulator>
 #include <osgGA/OrbitManipulator>
@@ -136,6 +137,7 @@ namespace
         bool is_public;
     };
     ManipulatorDefinition KNOWN_MANIPULATORS[] = {
+        { "Default", DEFAULT_MANIPULATOR, true },
         { "First Person", FIRST_PERSON_MANIPULATOR, true },
         { "Flight", FLIGHT_MANIPULATOR, true },
         { "Multi Touch Trackball", MULTI_TOUCH_TRACKBALL_MANIPULATOR, true },
@@ -1101,6 +1103,9 @@ void Vizkit3DWidget::setCameraManipulator(CAMERA_MANIPULATORS manipulatorType, b
     osg::ref_ptr<osgGA::CameraManipulator> newManipulator;
     switch(manipulatorType)
     {
+        case DEFAULT_MANIPULATOR:
+            newManipulator = new vizkit3d::DefaultManipulator;
+            break;
         case FIRST_PERSON_MANIPULATOR:
             newManipulator = new osgGA::FirstPersonManipulator;
             break;
