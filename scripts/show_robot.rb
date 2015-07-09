b27dc126f104030852ba57ae3292a5a5169dc082
Alexander Duda <Alexander.Duda@dfki.de>
2015-07-09 17:46:40 +0200

    add test script for robot visualization

---
+++ scripts/show_robot.rb
@@ -0,0 +1,37 @@
+begin
+    require 'pp'
+    require 'Qt4'
+    require 'qtuitools'
+rescue Exception => e
+    log = Logger.new(STDOUT)
+    log.error "!!! Cannot load Qt4 ruby bindings !!!"
+    raise e
+end
+
+app = Qt::Application.new(ARGV)
+loader = Qt::UiLoader.new()
+widget = loader.createWidget("vizkit3d::Vizkit3DWidget")
+plugin = widget.loadPlugin("vizkit3d","GridVisualization")
+robot = widget.loadPlugin("vizkit3d","RobotVisualization")
+robot.setModelPath("robot2.ive")
+robot.setVisualizationFrame("rover")
+
+timer = Qt::Timer.new
+angle = 0
+y = 0
+timer.connect SIGNAL(:timeout) do
+    x = -2+2.0 *Math::cos(angle)
+    quat = Qt::Quaternion::fromAxisAndAngle(0,0,1,0)
+    widget.setTransformation("world","rover",Qt::Vector3D.new(x,y,0.3),quat)
+    quat = Qt::Quaternion::fromAxisAndAngle(0,0,1,Math::cos(angle)*90)
+    robot.setJointRotation("pan_joint",quat)
+    quat = Qt::Quaternion::fromAxisAndAngle(0,1,0,(Math::cos(angle)*30))
+    robot.setJointRotation("tilt_joint",quat)
+    angle +=0.01
+end
+timer.start(10)
+
+widget.show
+app.exec
+
+
