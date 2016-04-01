b9f46df8fb910fb7073c4454ceabcbaf91ae054a
Steffen Planthaber <Steffen.Planthaber@dfki.de>
2016-04-01 15:49:31 +0200

    set the initial camera to valid value

--- src/Vizkit3DWidget.cpp
+++ src/Vizkit3DWidget.cpp
@@ -271,6 +271,9 @@ Vizkit3DWidget::Vizkit3DWidget( QWidget* parent,const QString &world_name,bool a
     addProperties(config,NULL);
 
     //setup camera
+    //currently only this is supproted
+    setCameraManipulator(TERRAIN_MANIPULATOR);
+
     osg::Camera* camera = window->getView()->getCamera();
     camera->setClearColor(::osg::Vec4(0.2, 0.2, 0.6, 1.0) );
     //camera->setViewport( new ::osg::Viewport(0, 0, traits->width, traits->height) );
