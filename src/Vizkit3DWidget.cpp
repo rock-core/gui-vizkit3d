1ed16b9052beeff2ed17445b95ebf5fcc154a3af
Pierre Willenbrock <pierre.willenbrock@dfki.de>
2026-01-26 14:53:22 +0100

    Use old VIZKIT_PLUGIN_RUBY_PATH in addition to new VIZKIT3D_PLUGIN_PATH
    This should prefer plugins installed via VIZKIT3D_PLUGIN_PATH, but
    generally, they should have either the same content, or one does
    not exist.

--- src/Vizkit3DWidget.cpp
+++ src/Vizkit3DWidget.cpp
@@ -1201,6 +1201,8 @@ QStringList* Vizkit3DWidget::getAvailablePlugins()
 
     QProcessEnvironment env = QProcessEnvironment::systemEnvironment();
     QString path_string = env.value("VIZKIT3D_PLUGIN_PATH","/usr/local/lib:/usr/lib");
+    //TODO remove VIZKIT_PLUGIN_RUBY_PATH once everything sets VIZKIT3D_PLUGIN_PATH
+    path_string += ":" + env.value("VIZKIT_PLUGIN_RUBY_PATH","/usr/local/lib:/usr/lib");
     QStringList paths = path_string.split(":");
     QStringList::iterator iter = paths.begin();
     for(;iter != paths.end();++iter)
@@ -1257,7 +1259,7 @@ QObject* Vizkit3DWidget::createPlugin(QString lib_name, QString plugin_name)
 
     if(path.isEmpty())
     {
-        std::cerr << "cannot find lib" + lib_name.toStdString()+"-viz in VIZKIT3D_PLUGIN_PATH." << std::endl;
+        std::cerr << "cannot find lib" + lib_name.toStdString()+"-viz in VIZKIT3D_PLUGIN_PATH or VIZKIT_PLUGIN_RUBY_PATH." << std::endl;
         return NULL;
     }
 
